33eac76bd5b02f135dda9e8759f1301f8c52d08e
Kay Altos <kayaltosnexus@gmail.com>
2023-05-01 14:05:57 +0300

    Добавил простейший интерфейс управления

--- main.py
+++ main.py
@@ -27,21 +27,64 @@ def render_pygame(field, scr):
 
 def main():
     gof = GameOfLife(30, 30)
-    gof.initialize(30)
+    gof.initialize(0)
 
     pygame.init()
+    pygame.font.init()
     screen = pygame.display.set_mode((640, 480))
     pygame.display.set_caption("Game of Life")
     clock = pygame.time.Clock()
     is_running = True
+    # В начале программы ставим игру на паузу
+    is_paused = True
+    # Переменная, в которой хранится шрифт
+    main_font = pygame.font.Font(None, 24)
+
     while is_running:
         for event in pygame.event.get():
             # проверить закрытие окна
             if event.type == pygame.QUIT:
                 is_running = False
-        gof.run_transition_rule()
+            # Если тип события - нажатая клавиша
+            if event.type == pygame.KEYDOWN:
+                # Если эта клавиша - пробел
+                if event.key == pygame.K_SPACE:
+                    # Если была пауза, то снимаем
+                    if is_paused == True:
+                        is_paused = False
+                    # Если паузы не было, то ставим на паузу
+                    else:
+                        is_paused = True
+            # Если нажата кнопка мыши
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # Записываем позицию курсора в пикселях
+                cursor_pos = event.pos
+                # Считаем индекс клетки по Х
+                x_pos = cursor_pos[0] // 15
+                # Считаем индекс клетки по Y
+                y_pos = cursor_pos[1] // 15
+                # Создаем переменную, в которую пишем новое состояние
+                # Сначала пишем туда текущее состояние клетки, что не было ошибок,
+                # если нажатие не будет обработано (например, нажата вторая кнопка мыши)
+                new_state = gof.field[y_pos][x_pos]
+                # Если нажата левая кнопка, то новое состояние - живая
+                if event.button == 1:
+                    new_state = 1
+                # Если нажата правая кнопка, то новое состояние - неживая
+                elif event.button == 3:
+                    new_state = 0
+                # Пишем в массив новое состояние клетки
+                gof.field[y_pos][x_pos] = new_state
+        # Если игра на паузе, то не обновляем состояние автомата, но рисовка
+        # все равно должна быть!
+        if is_paused == False:
+            gof.run_transition_rule()
         screen.fill((0, 0, 0))
         render_pygame(gof.field, screen)
+
+        text1 = main_font.render('Я люблю писать программы', True, (255, 255, 255))
+        screen.blit(text1, (10, 450))
+
         pygame.display.flip()
         # держим цикл на правильной скорости
         clock.tick(60)
